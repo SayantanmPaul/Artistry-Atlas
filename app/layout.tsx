8aefad4c02027ba18f91764a8526eded0fc2db20
Sayantan Paul <sayantan.paul9748@gmail.com>
2024-02-11 05:04:40 +0530

    feat: setup next-themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Provider } from "./utils/Provider";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* next theme provider via util.tsx */}
+        <Provider>
+          <main className="mx-auto max-w-[1120px]">
+            {children}
+            </main>
+        </Provider>
+      </body>
     </html>
   );
 }
