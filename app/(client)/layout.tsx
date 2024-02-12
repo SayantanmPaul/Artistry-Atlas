804ea109ee5c8469bb10eed482cb302f676ae051
Sayantan Paul <sayantan.paul9748@gmail.com>
2024-02-12 23:34:30 +0530

    chore: added sanity cms packege and change route groups

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Provider } from "./utils/provider"
+import { Provider } from "../utils/provider"
 import "./globals.css";
 import Navmenu from "@/components/Navmenu";
 
