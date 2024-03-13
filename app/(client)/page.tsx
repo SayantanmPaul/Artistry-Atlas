41b64b65d91301d917ecc37c1019db7d1d9461c3
Sayantanmpaul <sayantan.college6291@gmail.com>
2024-03-13 20:10:49 +0530

    temp: nextjs cache disable temporary home route

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import AboutSection from '@/app/sections/about-section'
-
+//nextjs cacheing set to false
+export const dynamic = 'force-dynamic'
 const Home = () => {
   return (
     <div>
