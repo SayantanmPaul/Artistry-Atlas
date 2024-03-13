7d3a0fbd1df8e9f637755d2b611e55f9166ae5e5
Sayantanmpaul <sayantan.college6291@gmail.com>
2024-03-13 21:43:01 +0530

    feat: download cv added
    desc: about-section (default route) initially completed !

--- app/sections/about-section.tsx
+++ app/sections/about-section.tsx
@@ -4,6 +4,9 @@ import { PortableText } from "@portabletext/react";
 import { About } from '@/types/about-type';
 import { aboutMySlef } from '@/sanity/sanity-queries';
 import { Roboto } from 'next/font/google';
+import { Button } from '@/components/ui/button';
+import { PiReadCvLogoBold } from "react-icons/pi";
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ["latin"] });
 const roboto = Roboto({ subsets: ['latin'], weight: '400' })
@@ -13,14 +16,25 @@ const AboutSection = async() => {
   return (
     <>
         <div className='max-w-[770px] flex flex-col lg:gap-[16px] gap-[4px]'>
-            <h1 className={`${inter.className} font-bold leading-[28px] text-[20px] lg:text-[24px] dark:text-[#F2F3F3] text-[#06030B]`}>About Me</h1>
-            <span className={`  text-[14px] dark:text-[#C4C4C4] text-[#494E52] lg:text-[16px] lg:leading-[27px] leading-[22px] text-pretty antialiased text-ellipsis font-medium`}>
+            <h1 className={`${inter.className} font-semibold leading-[28px] text-[20px] lg:text-[24px] dark:text-[#F2F3F3] text-[#06030B]`}>About Me</h1>
+            <span className={`${roboto.className}text-[14px] dark:text-[#C4C4C4] text-[#494E52] lg:leading-[27px] leading-[8px] text-pretty antialiased text-ellipsis font-medium`}>
                 {AboutDocs.map((data, i) => (
-                    <span key={i} className='prose dark:prose-invert lg:text-[16px] break-words text-wrap'>
+                    <span key={i} className='prose dark:prose-invert lg:text-[17.5px] text-[14px] break-words text-wrap list-none'>
                         <PortableText value={data.bio}/>
                     </span>
                 ))}
             </span>
+            <Link href="/sayantanpaul-resume.pdf" passHref legacyBehavior>
+                <a target="_blank" rel="noref">
+                <Button
+                    size='default'
+                    variant='default'
+                    className=' flex-initial inline-flex px-4 lg:w-auto md:w-auto font-semibold  rounded-sm lg:my-2 my-4 w-full'>
+                    Download CV
+                    <PiReadCvLogoBold size={20} className='ml-2 h-[16px] w-[16px]'/>
+                </Button>
+                </a>
+            </Link>
         </div>
     </>
 )}
