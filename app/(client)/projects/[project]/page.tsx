543af6d2549a02688a10a374aa0c57a299d11e06
Sayantanmpaul <sayantan.college6291@gmail.com>
2024-03-24 01:23:47 +0530

    fix: project metadata update

--- app/(client)/projects/[project]/page.tsx
+++ app/(client)/projects/[project]/page.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { projectDetails } from '@/sanity/sanity-queries'
 import ProjectDescSection from '@/app/sections/projectdesc-section';
-
+import { Metadata, ResolvingMetadata } from 'next';
 // export const dynamic = 'force-dynamic'
 
 type Props = {
   params: { project: string };
 };
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const slug = params.project;
+  const projectData = await projectDetails(slug);
+  return {
+    title: `Project: ${projectData.title}`,
+    description: `${projectData.metadescription}`,
+  };
+}
 
 const Project = async ({ params }: Props) => {
 
