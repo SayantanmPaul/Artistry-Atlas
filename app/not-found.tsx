a5357a8aaeb7f5aaba015b69cc51b889d88285d7
Sayantan Paul <sayantan.paul9748@gmail.com>
2024-03-25 22:22:40 +0530

    feat: custom 404 page build

---
+++ app/not-found.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import error from '@/public/assets/404.png'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+
+const NotFound = () => {
+  return (
+    <div className='flex flex-col w-full items-center gap-[36px] h-[70vh] justify-center duration-300 ease-in-out'>
+        <Image src={error} alt='404' width={340} height={340} className='lg:w-[340px] w-[186px]' />
+        <Link href={'/'}>
+            <Button className=' bg-rose-600 rounded-none font-bold'>Go back to Home</Button>
+        </Link>
+        <div className='w-full text-center'>
+            <p className=' text-[#494E52] dark:text-[#C4C4C4] text-[14px]'>This developer sometimes make dumb mistakes</p>
+            <a className=' text-[14px] font-semibold hover:underline text-rose-400' href="mailto:iampaulsayantan06@gamil.com">report this issue!</a>
+        </div>
+    </div>
+  )
+}
+
+export default NotFound
