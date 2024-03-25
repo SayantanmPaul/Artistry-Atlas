a0e0715d628ccc6ac8560f1950f200bf04b4059d
Sayantan Paul <sayantan.paul9748@gmail.com>
2024-03-25 21:11:55 +0530

    feat: linktree main socials add

--- app/sections/linktree-section.tsx
+++ app/sections/linktree-section.tsx
@@ -8,24 +8,65 @@ import { Project } from '@/types/project-type'
 import { projectList } from '@/sanity/sanity-queries'
 import { ImArrowUpRight2 } from "react-icons/im";
 import Link from 'next/link'
+import { FaSquareXTwitter, FaLinkedinIn, FaInstagram } from "react-icons/fa6";
+import { FiGithub } from "react-icons/fi";
+import { MdAlternateEmail } from "react-icons/md";
+import { mainSocials } from '../utils/mainSocials'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 const Ibarra= Ibarra_Real_Nova({weight:'600', style: 'normal', subsets: ['latin']})
 const poppins = Poppins({ weight: '400', style: 'normal', subsets: ['latin'] })
 const raleway= Raleway({ weight: '700', style: 'normal', subsets: ['latin'] })
 const roboto = Roboto({ subsets: ['latin'], weight: '400' })
 
+
+const SocialsLogo:{[key: string]: React.ElementType} = {
+  FaSquareXTwitter: FaSquareXTwitter,
+  FaLinkedinIn: FaLinkedinIn,
+  FiGithub: FiGithub,
+  FaInstagram: FaInstagram,
+  MdAlternateEmail: MdAlternateEmail
+}
+
 const LinktreeSection = async() => {
   const otherLinklist: Links[] = await linkList()
   const featureProjectList: Project[] = await projectList()
   
   return (
-    <div className='flex flex-col gap-[36px] items-center'>
-      <div className='flex flex-col gap-[16px] items-center'>
+    <div className='flex flex-col lg:gap-[36px] gap-[28px] items-center'>
+      <div className='flex flex-col lg:gap-[16px] gap-[12px] items-center'>
         <div className='border border-[#005BEA] rounded-full p-1'>
         <Image src={myself} alt='sayantan paul' width={250} height={250} className='w-[112px] h-[112px] rounded-full overflow-hidden'/>
         </div>
         <h2 className={`${Ibarra.className} text-[24px] leading-[22px] font-bold`}>Sayantan paul</h2>
         <p className={`${poppins.className} text-[12px] leading-[22px] font-normal max-w-[264px] text-center text-[#494E52] dark:text-[#C4C4C4]`}>Software Developer | Machine Learning | An Part-Time Artist :)</p>
+      <div className='flex flex-row gap-[24px]'>
+        {mainSocials&& SocialsLogo && mainSocials.map((link,i)=>{
+          const IconComp = SocialsLogo[link.logo]
+          return IconComp ? (
+            <TooltipProvider key={i}>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                <Link href={link.link}>
+                  <div key={i} className='p-[10px] rounded-[8px] border border-transparent hover:border-[#005BEA] ease-in-out duration-300 hover:text-[#005BEA] dark:hover:text-[#00C6FB]'>
+                  <IconComp size={20}/>
+                  </div>
+                </Link>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>{link.name}</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+            
+          ): null
+        })}
+      </div>
       </div>
       <div className='flex flex-col gap-[20px]'>
         <p className={`${raleway.className} tracking-[6px] dark:text-white/60 uppercase text-[14px] text-[#191825]/60 duration-300 ease-in-out text-center w-full`}>Featured Projects</p>
@@ -37,7 +78,7 @@ const LinktreeSection = async() => {
                   <div className='dark:bg-[#0F0B17] bg-[#FFFFFF] rounded-[5px]'>
                     <div className='flex flex-row justify-between items-center px-[22px] py-[8px]'>
                       <div className='flex flex-row gap-[12px] items-center'>
-                        <Image src={project.logo} alt={project._id} width={16} height={16} className='w-[16px] h-auto object-fill' />
+                        <Image style={{width: 'auto', height: 'auto'}} src={project.logo} alt={project._id} width={16} height={16} className=' w-auto h-auto object-fill' />
                         <p className=' text-[14px] leading-[20px] font-semibold'>{project.title}</p>
                       </div>
                       <ImArrowUpRight2 size={16} className='text-[#D0CADF] dark:text-[#F2F3F3]/40 group-hover:text-[#1798FF] duration-300 ease-in-out' />
---
+++ app/utils/mainSocials.ts
@@ -0,0 +1,36 @@
+type Socials={
+    name: string,
+    link: string,
+    logo: string,
+    color?: string
+}
+
+export const mainSocials:Socials[] = [
+    {
+        name: 'Twitter',
+        link: 'https://twitter.com/impaul_p78814',
+        logo: 'FaSquareXTwitter'
+    },
+    {
+        name: 'LinkedIn',
+        link: 'https://www.linkedin.com/in/imsayantanpaul/',
+        logo: 'FaLinkedinIn',
+        color: 'text-[#0966c2]'
+    },
+    {
+        name: 'Github',
+        link: 'https://github.com/SayantanmPaul',
+        logo: 'FiGithub'
+    },
+    {
+        name: 'Instagram',
+        link: 'https://www.instagram.com/sayantanpaul.11',
+        logo: 'FaInstagram',
+        color: 'text-[#FC00FF]'
+    },
+    {
+        name: 'Email',
+        link: 'mailto:iam.paulsayantan06@gmail.com',
+        logo: 'MdAlternateEmail',
+    },
+]
